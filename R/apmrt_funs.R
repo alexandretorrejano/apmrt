ddd41fdaa0be7c7c4caa0fb91caa23aa7e9922e4
TORREJANO Alexandre Pereira <APTORREJANO@Worten.pt>
2020-05-07 14:27:54 +0100

    added functions:
    -fstart calls in the beginning of a script to set enviornment and working directory options and install packages;
    
    -fwipe wipes temporary data;
    
    fload loads data/objects from files;
    
    -fsave saves data/objects to file;
    
    -fquery queries an existing database connection;
    
    -fsub replaces patterns in strings;

---
+++ R/apmrt_funs.R
@@ -0,0 +1,82 @@
+fstart<-function(){
+  setwd(dirname(rstudioapi::getSourceEditorContext()$path)) # Sets working directory to caller script's location
+  
+  if(!require(pacman)){install.packages("pacman")} # Guarantees that pacman is installed for handling of other packages
+  options(scipen=999)
+  pacman::p_load(backports,bit64,cluster,data.table,DBI,DMwR,doParallel,dplyr,factoextra,foreach,ggplot2,gmodels,kableExtra,knitr,kohonen,lubridate,maditr,NbClust,odbc,openxlsx,parallel,progress,readxl,reshape,RGoogleAnalytics,rmarkdown,stringr,WriteXLS) # Installs
+}
+
+fwipe<-function(){
+    closeAllConnections()
+    rm(list=ls())
+    tryCatch(dev.off(),error=function(e){NULL})
+    gc()
+    cat("\14Environment Wiped.\n")
+}
+
+
+fload<-function(f,...){
+  ext<-f%>%strsplit("\\.")%>%unlist
+  
+  if(length(ext)>1){
+    ext<-tail(ext,1)
+  } else{
+    ext<-"Rds"
+  }
+  
+  lext<-tolower(ext)
+  
+  if(lext=="csv"){
+    d<-fread(f,integer64="character",...)
+  } else if(lext=="xlsx"){
+    d<-read.xlsx(f,...)%>%data.table
+  } else if(lext=="xls"){
+    d<-read_xls(f,...)%>%data.table
+  } else if(lext=="txt"){
+    d<-read.table(f,...)%>%data.table
+  } else if(lext=="rds"){
+    d<-readRDS(f,...)
+  } else{
+    stop(paste0("FILE ERROR: UNKNOWN EXTENSION FOR READING: .",ext,"\n"))
+  }
+  
+  return(d)
+} # Reads data from files and ensures data.table format with correct column types unless it's a RDS save in which case it reads the object as is
+
+
+fsave<-function(d,f,...){
+  ext<-f%>%strsplit("\\.")%>%unlist
+  
+  if(length(ext)>1){
+    ext<-tail(ext,1)
+  } else{
+    ext<-"Rds"
+    f<-paste0(f,".",ext)
+  }
+  
+  lext<-tolower(ext)
+  
+  if(lext=="csv"){
+    d<-fwrite(d,f,...)
+  } else if(lext=="xlsx"){
+    d<-write.xlsx(d,f,...)%>%data.table
+  } else if(lext=="txt"){
+    d<-write.table(d,f,...)%>%data.table
+  } else if(lext=="rds"){
+    d<-saveRDS(d,f,...)
+  } else{
+    stop(paste0("FILE ERROR: UNKNOWN EXTENSION FOR WRITING: .",ext,"\n"))
+  }
+} # Writes data/object to file
+
+fquery<-function(con,q){
+  suf<-ifelse(grepl("\r\n",q),' (...)','')
+  cat(paste0("Running Query [",gsub("\n.*",'',q)%>%gsub(pattern='\r',replacement=''),suf,"]: "))
+  x<-dbGetQuery(con,q)
+  cat(paste0("Success\n"))
+  return(x)
+}
+
+fsub<-function(x,pattern,replacement,ignore.case = FALSE, perl = FALSE, fixed = FALSE, useBytes = FALSE){
+  gsub(pattern, replacement, x, ignore.case, perl,fixed,useBytes)
+}
