df6fd7387fc5619dfb99df3fecd466b41882886a
TORREJANO Alexandre Pereira <APTORREJANO@Worten.pt>
2020-05-07 14:34:38 +0100

    added package names to function calls and package installation on top of the script

--- R/apmrt_funs.R
+++ R/apmrt_funs.R
@@ -1,6 +1,9 @@
+if(!require(pacman)){install.packages("pacman")}
+pacman::p_load(backports,bit64,cluster,data.table,DBI,DMwR,doParallel,dplyr,factoextra,foreach,ggplot2,gmodels,kableExtra,knitr,kohonen,lubridate,maditr,NbClust,odbc,openxlsx,parallel,progress,readxl,reshape,RGoogleAnalytics,rmarkdown,stringr,WriteXLS)
+
 fstart<-function(){
   setwd(dirname(rstudioapi::getSourceEditorContext()$path)) # Sets working directory to caller script's location
-  
+
   if(!require(pacman)){install.packages("pacman")} # Guarantees that pacman is installed for handling of other packages
   options(scipen=999)
   pacman::p_load(backports,bit64,cluster,data.table,DBI,DMwR,doParallel,dplyr,factoextra,foreach,ggplot2,gmodels,kableExtra,knitr,kohonen,lubridate,maditr,NbClust,odbc,openxlsx,parallel,progress,readxl,reshape,RGoogleAnalytics,rmarkdown,stringr,WriteXLS) # Installs
@@ -17,51 +20,51 @@ fwipe<-function(){
 
 fload<-function(f,...){
   ext<-f%>%strsplit("\\.")%>%unlist
-  
+
   if(length(ext)>1){
     ext<-tail(ext,1)
   } else{
     ext<-"Rds"
   }
-  
+
   lext<-tolower(ext)
-  
+
   if(lext=="csv"){
-    d<-fread(f,integer64="character",...)
+    d<-data.table::fread(f,integer64="character",...)
   } else if(lext=="xlsx"){
-    d<-read.xlsx(f,...)%>%data.table
+    d<-openxlsx::read.xlsx(f,...)%>%data.table
   } else if(lext=="xls"){
-    d<-read_xls(f,...)%>%data.table
+    d<-readxl::read_xls(f,...)%>%data.table
   } else if(lext=="txt"){
-    d<-read.table(f,...)%>%data.table
+    d<-utils::read.table(f,...)%>%data.table
   } else if(lext=="rds"){
     d<-readRDS(f,...)
   } else{
     stop(paste0("FILE ERROR: UNKNOWN EXTENSION FOR READING: .",ext,"\n"))
   }
-  
+
   return(d)
 } # Reads data from files and ensures data.table format with correct column types unless it's a RDS save in which case it reads the object as is
 
 
 fsave<-function(d,f,...){
   ext<-f%>%strsplit("\\.")%>%unlist
-  
+
   if(length(ext)>1){
     ext<-tail(ext,1)
   } else{
     ext<-"Rds"
     f<-paste0(f,".",ext)
   }
-  
+
   lext<-tolower(ext)
-  
+
   if(lext=="csv"){
-    d<-fwrite(d,f,...)
+    d<-data.table::fwrite(d,f,...)
   } else if(lext=="xlsx"){
-    d<-write.xlsx(d,f,...)%>%data.table
+    d<-openxlsx::write.xlsx(d,f,...)%>%data.table
   } else if(lext=="txt"){
-    d<-write.table(d,f,...)%>%data.table
+    d<-utils::write.table(d,f,...)%>%data.table
   } else if(lext=="rds"){
     d<-saveRDS(d,f,...)
   } else{
@@ -72,7 +75,7 @@ fsave<-function(d,f,...){
 fquery<-function(con,q){
   suf<-ifelse(grepl("\r\n",q),' (...)','')
   cat(paste0("Running Query [",gsub("\n.*",'',q)%>%gsub(pattern='\r',replacement=''),suf,"]: "))
-  x<-dbGetQuery(con,q)
+  x<-DBI::dbGetQuery(con,q)
   cat(paste0("Success\n"))
   return(x)
 }
