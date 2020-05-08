1c020d3d3f4b4922da4cb66ea2c06ea01bd010fd
TORREJANO Alexandre Pereira <APTORREJANO@Worten.pt>
2020-05-08 15:30:46 +0100

    antoher minor change to go() printed message

--- R/apmrt_funs.R
+++ R/apmrt_funs.R
@@ -9,7 +9,7 @@ go<-function(){
   #if(!require(pacman)){install.packages("pacman")}
   options(scipen=999)
   pacman::p_load(backports,bit64,cluster,data.table,DBI,DMwR,doParallel,dplyr,factoextra,foreach,ggplot2,gmodels,kableExtra,knitr,kohonen,lubridate,maditr,NbClust,odbc,openxlsx,parallel,progress,readxl,reshape,RGoogleAnalytics,rlang,rmarkdown,stringr,WriteXLS)
-  cat("\nOptions Set.\n")
+  cat("Options Set.\n")
 }
 
 
