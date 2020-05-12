2f94ba41797daa30473f0a2483506a4d3909def8
TORREJANO Alexandre Pereira <APTORREJANO@Worten.pt>
2020-05-12 16:48:49 +0100

    refined installer/updater script

--- apmrt_install_update.R
+++ apmrt_install_update.R
@@ -1,4 +1,12 @@
-if(require("apmrt")){remove.packages("apmrt")}
-devtools::install_github("alexandretorrejano/apmrt")
-library(apmrt)
-packageVersion("apmrt")
+{
+  if(require("apmrt")){
+    ver1<-packageVersion("apmrt")
+    remove.packages("apmrt")
+    }
+  devtools::install_github("alexandretorrejano/apmrt")
+  library(apmrt)
+
+  cat(paste("\nVersion as of Script Call:",ver1))
+  cat(paste("\nNewly Installed Version:  ",packageVersion("apmrt")))
+  rm(ver1)
+}
